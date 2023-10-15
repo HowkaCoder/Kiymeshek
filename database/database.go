93b31799e7e88f82a1b10ba87bb4ce92b9c8a223
HowkaCoder <prostojproger@gmail.com>
2023-10-15 23:28:30 +0500

    railway add

--- database/database.go
+++ database/database.go
@@ -1,29 +1,37 @@
-package database 
- 
-import ( 
- "fmt" 
- "go-auth/models" 
- "log"  
- "gorm.io/driver/postgres" 
- "gorm.io/gorm" 
-) 
- 
-var DB *gorm.DB 
- 
-func ConnectToDB() { 
- var err error 
- dsn := "host=192.168.88.126 user=howka password=euF@I16TuradPUXF dbname=kiymeshek port=3306 sslmode=disable TimeZone=Asia/Shanghai"
+package database
 
- DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{}) 
- if err != nil { 
-  log.Fatal(err) 
- } 
- DB.AutoMigrate(&models.User{}) 
- DB.AutoMigrate(&models.Ava{}) 
- DB.AutoMigrate(&models.Photo{}) 
- DB.AutoMigrate(&models.Video{}) 
- DB.AutoMigrate(&models.Category{}) 
- DB.AutoMigrate(&models.Post{}) 
- fmt.Println("Database Migrated") 
- 
+import (
+	"fmt"
+	"go-auth/config"
+	"go-auth/models"
+	"log"
+	"strconv"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	p := config.Config("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(containers-us-west-163.railway.app:%d)/%s?charset=utf8mb4&parseTime=true&loc=Local", config.Config("DB_USERNAME"), config.Config("DB_PASSWORD"), port, config.Config("DB_NAME"))
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.Ava{})
+	DB.AutoMigrate(&models.Photo{})
+	DB.AutoMigrate(&models.Video{})
+	DB.AutoMigrate(&models.Category{})
+	DB.AutoMigrate(&models.Post{})
+	fmt.Println("Database Migrated")
+
 }
